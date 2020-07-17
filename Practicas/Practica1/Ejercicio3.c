36e341ec0464901b30cb89f202fd4b34daf9ba7d
alesamv <disasterology03@hotmail.com>
2020-07-17 13:36:44 -0500

    Tabla de Pitagoras Tridimensional

---
+++ Practicas/Practica1/Ejercicio3.c
@@ -0,0 +1,21 @@
+/*Este programa imprime una tabla pitagórica tridimensional, usando un arreglo
+tridimensional.*/
+#include<stdio.h>
+int main()
+{
+	int iNumero[10][10][10],i,j,k;
+	printf("\tTabla de multiplicaciones\n");
+	for (i=0;i<10;i++)
+	{
+		for(j=0;j<10;j++)
+		{
+			for(k=0;k<10;k++)
+			{
+				iNumero[i][j][k]=(i+1)*(j+1)*(k+1);
+				printf("%d\t",iNumero[i][j][k]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+}
