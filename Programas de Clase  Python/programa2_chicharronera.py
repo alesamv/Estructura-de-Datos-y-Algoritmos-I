7533ccea7dd7abd0d962bd8685091ae934f9ccac
alesamv <alesamv@gmail.com>
2020-12-03 12:24:17 -0600

    Adding Files

---
+++ Programas de Clase  Python/programa2_chicharronera.py
@@ -0,0 +1,14 @@
+import math
+print("Resolucion a una ecuacion de 2do grado")
+a = 1
+b = -5
+c= 6
+
+d = math.sqrt((b**2)-(4*a*c))
+if d < 0:
+    print("El resultado no se encuentra en los reales")
+else:
+    x1 = ((-b)+d)/(2*a)
+    print("x1 = ",x1)
+    x2 = ((-b)-d)/(2*a)
+    print("x2 = ",x2)
