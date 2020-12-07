0c14e410f1958735aaab53be0c2bdd581467011a
alesamv <alesamv@gmail.com>
2020-12-07 16:49:54 -0600

    Adding File

---
+++ Programas de Clase  Python/programa6_list.py
@@ -0,0 +1,9 @@
+L = [1,2,3,'par', ['Py',['t','h','o','n']]]
+
+print(L[0])         #1
+print(L[2])         #3
+print(L[3])         #par
+print(L[3][2])      #r
+print(L[4][0])      #Py
+print(L[4][0][1])   #y
+print(L[4][1][1])   #h
