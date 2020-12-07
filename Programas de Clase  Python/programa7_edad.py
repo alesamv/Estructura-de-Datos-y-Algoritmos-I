45a9431efa9ed9b5ed15aad2d1a60ca57ff47084
alesamv <alesamv@gmail.com>
2020-12-07 16:57:11 -0600

    Adding File

---
+++ Programas de Clase  Python/programa7_edad.py
@@ -0,0 +1,6 @@
+print("Introduce tu edad: ")
+edad = int(input())
+if (edad >= 6 and edad <=12) or edad >=60:
+    print("Usted califica para los descuentos")
+else:
+    print("Usted no aplica :(")
