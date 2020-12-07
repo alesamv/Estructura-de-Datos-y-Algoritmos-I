e21cf65406229209d46b72db520eec068646e186
alesamv <alesamv@gmail.com>
2020-12-07 16:42:27 -0600

    Adding File

---
+++ Programas de Clase  Python/programa5_slicing.py
@@ -0,0 +1,15 @@
+c = "mantequilla"
+print(c[0])     #m
+print(c[4])     #e
+print(c[10])    #a
+#print(c[12])    #error
+print(c[-1])    #a
+print(c[-5])    #u
+print(c[0:4])   #mant
+print(c[3:8])   #tequi
+print(c[4:14])  #equilla
+print(c[-4:-2]) #il
+print(c[:4])    #mant
+print(c[3:])    #tequilla
+print(c[:])     #mantequilla
+print(c[-5:])   #uilla
