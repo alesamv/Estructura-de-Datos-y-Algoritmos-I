708cfc5dc167fad3b854bd80a0d8fde71c82ba8a
alesamv <alesamv@gmail.com>
2020-12-12 17:11:39 -0600

    Adding Files

--- Programas de Clase  Python/programa11_whileCuadrado.py
+++ Programas de Clase  Python/programa11_whileCuadrado.py
@@ -1,9 +1,9 @@
 #Cuadrado de 3*3
 
 i=0
-while i<=3:
-    j=1
-    while j<=3:
+while i<3:
+    j=0
+    while j<3:
         print("*"),
         j+=1
     print("\n")
---
+++ Programas de Clase  Python/programa15_forCuadrado.py
@@ -0,0 +1,6 @@
+#Cuadrado de 3*3
+
+for i in range(0,3):
+    for i in range(0,3):
+        print("*"),
+    print("\n")
